75416ebc80386bb248ac2acaa0516b00be77ab0a
ke981015 <78714008+ke-alaric@users.noreply.github.com>
2023-09-07 10:16:13 -0400

    delete

--- src/main/java/com/marlabs/poc/ProofOfConceptApplication.java
+++ src/main/java/com/marlabs/poc/ProofOfConceptApplication.java
@@ -2,24 +2,9 @@ package com.marlabs.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class ProofOfConceptApplication {
-	
-    @Bean
-    LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
-    RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String>  redisTemplate =  new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
-        return redisTemplate;
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProofOfConceptApplication.class, args);
