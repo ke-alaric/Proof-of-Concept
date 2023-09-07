eb04d0912bb07376501be827deb79b048877af87
ke981015 <78714008+ke-alaric@users.noreply.github.com>
2023-09-07 12:46:46 -0400

    user constants

---
+++ src/main/java/com/marlabs/poc/constants/UserConstants.java
@@ -0,0 +1,12 @@
+package com.marlabs.poc.constants;
+
+public class UserConstants {
+    public static final String USER_OBJECT_TYPE = "user";
+    public static final String OBJECT_TYPE_NAME = "objectType";
+    public static final String OBJECT_ID_MAME = "objectId";
+    public final static String REDIS_URL = "localhost";
+    public final static int PORT = 6379;
+    public static final String PRE_ID_DELIMITER = ":";
+    public static final String PRE_FIELD_DELIMITER = ">>";
+    public static final String REDIS_ALL_PATTERN = "*";
+}
